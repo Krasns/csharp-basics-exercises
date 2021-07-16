faa565e5c4177cd6f2cdd4ac02c11ce22a06b8c6
Matīss Ceplītis <matissceplitis051@gmail.com>
2021-07-16 13:42:16 +0300

    reworked

--- TypesAndVariables/VariablesAndNames/Program.cs
+++ TypesAndVariables/VariablesAndNames/Program.cs
@@ -6,21 +6,21 @@ namespace VariablesAndNames
     {
         private static void Main(string[] args)
         {
-            int Cars, Drivers, Passengers, Cars_Not_Driven, Carpool_Capacity, Average_Passengers_Per_Car;
+            int Cars, Drivers, Passengers, Cars_Not_Driven, Capacity, Passengers_Per_Car;
 
             Cars = 100;
             Drivers = 2;
             Passengers = 90;
             Cars_Not_Driven = 10;
-            Carpool_Capacity = 16;
-            Average_Passengers_Per_Car = 4;
+            Capacity = 16;
+            Passengers_Per_Car = 3;
 
             Console.WriteLine("There are " + Cars + " cars available.");
             Console.WriteLine("There are only " + Drivers + " drivers available.");
             Console.WriteLine("There will be " + Cars_Not_Driven + " empty cars today.");
-            Console.WriteLine("We can transport " + Carpool_Capacity + " people today.");
+            Console.WriteLine("We can transport " + Capacity + " people today.");
             Console.WriteLine("We have " + Passengers + " to carpool today.");
-            Console.WriteLine("We need to put about" + Average_Passengers_Per_Car + " in each car.");
+            Console.WriteLine("We need to put about " + Passengers_Per_Car + " in each car.");
             Console.ReadKey();
         }
     }
