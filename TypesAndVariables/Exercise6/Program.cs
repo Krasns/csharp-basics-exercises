9c60c1af4e47f705bc7c4f9129f783f2cbd89058
Matīss Ceplītis <matissceplitis051@gmail.com>
2021-07-16 15:00:55 +0300

    Exercise6 done

--- TypesAndVariables/Exercise6/Program.cs
+++ TypesAndVariables/Exercise6/Program.cs
@@ -6,7 +6,18 @@ namespace Exercise6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter numbers: ");
+            int AddNumber = Convert.ToInt32(Console.ReadLine());
+            int SumNumber = 0;
+
+            while (AddNumber !=0)
+            {
+                SumNumber += AddNumber % 10;
+                AddNumber /= 10;
+            }
+            
+            Console.WriteLine(SumNumber);
+            Console.ReadKey();
         }
     }
 }
