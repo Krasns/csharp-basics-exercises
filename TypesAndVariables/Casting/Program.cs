4b8897e0349e625f3094152a508917b6d84a6c94
Matīss Ceplītis <matissceplitis051@gmail.com>
2021-07-16 13:42:39 +0300

    Casting done

--- TypesAndVariables/Casting/Program.cs
+++ TypesAndVariables/Casting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,11 +18,14 @@ namespace Casting
 
         static void first()
         {
-            String a = "1";
+            string a = "1";
             int b = 2;
             int c = 3;
             double d = 4;
             float e = 5;
+
+            int sum = Convert.ToInt32(int.Parse(a) + b + c + d + e);
+            Console.WriteLine(sum);
 
             //fixme - should be 15 :|
             //int sum = a + b + c + d + e;
@@ -35,6 +39,11 @@ namespace Casting
             int c = 3;
             double d = 4.2;
             float e = 5.3f;
+
+            int.Parse(a);
+
+            float sum = int.Parse(a) + b + c + (float)d + e;
+            Console.WriteLine(sum);
 
             //fixme - should be 15.5 :| 
             //float sum = a + b + c + d + e;
