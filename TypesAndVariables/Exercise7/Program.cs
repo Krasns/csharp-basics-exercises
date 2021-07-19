43dc24dffe80c19cc4fb43b8203775c2756cd9c1
Matīss Ceplītis <matissceplitis051@gmail.com>
2021-07-19 13:28:29 +0300

    Exercise7 done

---
+++ TypesAndVariables/Exercise7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name;
+            char[] Arrayname;
+            int Count;
+
+            Console.Write("Enter a string: ");
+            Name = Console.ReadLine();
+            Arrayname = Name.ToCharArray();
+            Count = 0;
+
+            foreach (char Letter in Arrayname)
+            {
+                if (char.IsUpper(Letter))
+                    Count++;
+            }
+
+            Console.WriteLine($"There is {Count} upper letters.");
+            Console.ReadKey();
+        }
+    }
+}
