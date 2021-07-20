a2919302c6c4ef218a01c0b0eeb1a018bf66ba13
Matīss Ceplītis <matissceplitis051@gmail.com>
2021-07-20 15:49:08 +0300

    Exercise4 done

---
+++ Arithmetic/Exercise4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number, Sum;
+
+            Console.WriteLine("Enter number: ");
+            Number = Convert.ToInt32(Console.ReadLine());
+            Sum = 1;
+
+            for (int i = 1; i < Number; i++)
+            {
+                Sum += Sum * i;
+            }
+
+            Console.WriteLine($"Number from 1 to {Number} is {Sum}");
+            Console.ReadKey();
+        }
+    }
+}
