5107c3b7d79100833ee6fed7cc9a033c1e157c5e
Matīss Ceplītis <matissceplitis051@gmail.com>
2021-07-29 19:33:33 +0300

    TenBillion comments del

--- FlowOfControl/FlowControl/TenBillion/Program.cs
+++ FlowOfControl/FlowControl/TenBillion/Program.cs
@@ -4,7 +4,6 @@ namespace TenBillion
 {
     class Program
     {
-        //TODO Write a C# program that reads an positive integer and count the number of digits the number (less than ten billion) has.
         static void Main(string[] args)
         {
             Console.WriteLine("Input an integer number less than ten billion: ");
