191397e527a98f58f7c458d5dffc5fa6b1b2f28a
Matīss Ceplītis <matissceplitis051@gmail.com>
2021-07-29 23:22:35 +0300

    Exercise1 cleared up

--- Arrays/Exercise1/Program.cs
+++ Arrays/Exercise1/Program.cs
@@ -24,14 +24,13 @@ namespace Exercise1
             };
             
             Console.WriteLine("Original numeric array : " + string.Join("," , myArray1));
-
             Array.Sort(myArray1);
             Console.WriteLine("Sorted numeric array : " + string.Join("," , myArray1));
     
             Console.WriteLine("Original string array : " + string.Join("," , myArray2));
-
             Array.Sort(myArray2);
             Console.WriteLine("Sorted string array : " + string.Join("," , myArray2));
+
             Console.ReadKey();
         }
     }
