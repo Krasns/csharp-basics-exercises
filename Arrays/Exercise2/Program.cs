85ce425c1474c2c6f10b0116c20c23414c12fe8b
Matīss Ceplītis <matissceplitis051@gmail.com>
2021-08-02 13:47:52 +0300

    Exercise2 done

--- Arrays/Exercise2/Program.cs
+++ Arrays/Exercise2/Program.cs
@@ -4,23 +4,33 @@ namespace Exercise2
 {
     class Program
     {       
-        //TODO: Write a C# program to sum values of an array.
         private static void Main(string[] args)
         {
             int[] myArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
             var sum = 0;
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                sum += myArray[i];
+            }
+
+            Console.WriteLine("The sum is " + sum);
+            Console.ReadKey();
 
             Console.WriteLine("Please enter a min number");
             int minNumber = int.Parse(Console.ReadLine());
             Console.WriteLine("Please enter a max number");
             int maxNumber = int.Parse(Console.ReadLine());
 
-            /*
-            fixme
-            for (........) {
-            ........
+            int[] secendArray = new int[maxNumber-minNumber];
+            sum = 0;
+
+            secendArray[0] = minNumber;
+            for (int i = 0; i < secendArray.Length; i++)
+            {
+                secendArray[i] = i + secendArray[0];
+                sum += secendArray[i];
             }
-            */
 
             Console.WriteLine("The sum is " + sum);
             Console.ReadKey();
