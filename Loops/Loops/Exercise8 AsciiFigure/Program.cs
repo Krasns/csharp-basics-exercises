db2ad8ccfc80787562bd4c6b0c1d20a00ac93e3c
Matīss Ceplītis <matissceplitis051@gmail.com>
2021-08-04 16:15:06 +0300

    Exercise8_AsciiFigure second line dosent show lest \

---
+++ Loops/Loops/Exercise8 AsciiFigure/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Exercise8_AsciiFigure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 8*2; j++)
+                {
+                    if (i == 0 && j < 8)
+                    {
+                        Console.Write("/");
+                    }
+                    else if (i == 0 && j < 16)
+                    {
+                        Console.Write("\\");
+                    }
+                    else if (i == 1 && j < 4)
+                    {
+                        Console.Write("/");
+                    }
+                    else if (i == 1 && j > 4 && j <= 12)
+                    {
+                        Console.Write("*");
+                    }
+                    else if (i == 1 && j > 12 && j <= 16)
+                    {
+                        Console.Write("\\");
+                    }
+                    else if (i == 2)
+                    {
+                        Console.Write("*");
+                    }
+
+                }
+                    Console.WriteLine();
+            }
+        }
+    }
+}
