19b66ad95d190585ba1983cb5ba2276f7fe283bf
Matīss Ceplītis <matissceplitis051@gmail.com>
2021-08-04 15:19:44 +0300

    Exercise7_Piglet done

---
+++ Loops/Loops/Exercise7 Piglet/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercise7_Piglet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Piglet!");
+            Random rand = new Random();
+            int number = rand.Next(1, 7);
+            int sum = 0;
+
+            if (number == 1)
+            {
+                Console.WriteLine($"You rolled {number}");
+            }
+
+            string answer = "";
+            while (answer != "no" && number != 1)
+            {
+                number = rand.Next(1,7);
+                sum += number;
+                Console.WriteLine($"You rolled {number}");
+                Console.Write("Roll again? ");
+                answer = Console.ReadLine();
+            }
+            if (number == 1)
+            {
+                Console.WriteLine("You got 0 points.");
+            }
+            else
+            {
+                Console.WriteLine($"You got {sum} points.");
+            }
+        }
+    }
+}
