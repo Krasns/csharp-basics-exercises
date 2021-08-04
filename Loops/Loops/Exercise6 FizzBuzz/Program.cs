84bc5f86fb88a10874b1590b536bd66d3eea4f8e
Matīss Ceplītis <matissceplitis051@gmail.com>
2021-08-04 14:53:03 +0300

    Exercise6_FizzBuzz done

---
+++ Loops/Loops/Exercise6 FizzBuzz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Exercise6_FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Max value?");
+            int max = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < max; i++)
+            {
+                if ((i-1) % 20 == 0)
+                {
+                    Console.WriteLine();
+                }
+
+                if (i%15==0)
+                {
+                    Console.Write("FizzBuzz ");
+                }
+                else if (i%3==0)
+                {
+                    Console.Write("Fizz ");
+                }
+                else if (i%5==0)
+                {
+                    Console.Write("Buzz ");
+                }
+                else
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
