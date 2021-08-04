8a86ed782cce46e8b1ffa12e8161212bbbf2b5c2
Matīss Ceplītis <matissceplitis051@gmail.com>
2021-08-04 21:27:13 +0300

    Exercise9 done

---
+++ Loops/Loops/Exercise9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Desired sum: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Random rand = new Random();
+            int sum = 0;
+
+            while (number != sum)
+            {
+                int first = rand.Next(1, 7);
+                int second = rand.Next(1, 7);
+                Console.WriteLine($"{first} and {second} = {sum = first + second}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
