7c871f62d2c5f9cbcee40bcd1ed60c6ac811112b
Matīss Ceplītis <matissceplitis051@gmail.com>
2021-08-11 17:46:49 +0300

    Vajadzētu būt visam

--- Arithmetic/CalculateArea/Geometry.cs
+++ Arithmetic/CalculateArea/Geometry.cs
@@ -10,17 +10,20 @@ namespace CalculateArea
     {
         public static double areaOfCircle(decimal radius)
         {
-            throw new NotImplementedException();
+            double sum = Math.PI * Math.Pow(Convert.ToDouble(radius), 2);
+            return Math.Round(sum, 2);
         }
 
         public static double areaOfRectangle(decimal length, decimal width)
         {
-            throw new NotImplementedException();
+            decimal sum = length * width;
+            return Convert.ToDouble(Math.Round(sum, 2));
         }
 
         public static double areaOfTriangle(decimal ground, decimal h)
         {
-            throw new NotImplementedException();
+            double sum = Convert.ToDouble(ground) * Convert.ToDouble(h) * 0.5;
+            return Math.Round(sum, 2);
         }
     }
 }
