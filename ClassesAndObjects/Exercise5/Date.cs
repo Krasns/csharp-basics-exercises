24a5ff3cc3c7d004f22a1d6dd4e1f788b505e88e
Matīss Ceplītis <matissceplitis051@gmail.com>
2021-08-18 17:14:29 +0300

    Exercise5 done

---
+++ ClassesAndObjects/Exercise5/Date.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5
+{
+    class Date
+    {
+        string _day;
+        string _month;
+        string _year;
+        public Date(string day, string month, string year)
+        {
+            _day = day;
+            _month = month;
+            _year = year;
+        }
+        public string Day
+        {
+            get => _day;
+            set => _day = value;
+        }
+        public string Month
+        {
+            get => _month;
+            set => _month = value;
+        }
+        public string Year
+        {
+            get => _year;
+            set => _year = value;
+        }
+
+        public void DisplayDate() 
+        {
+            Console.WriteLine($"{_month}/{_day}/{_year}");
+        }
+    }
+}
---
+++ ClassesAndObjects/Exercise5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter day:");
+            string day = Console.ReadLine();
+
+            Console.WriteLine("Enter month:");
+            string month = Console.ReadLine();
+
+            Console.WriteLine("Enter year:");
+            string year = Console.ReadLine();
+
+            var date = new Date(day, month, year);
+            date.DisplayDate();
+            Console.ReadKey();
+        }
+    }
+}
