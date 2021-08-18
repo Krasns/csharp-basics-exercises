16c6ebeaa53539b8c873a97e414c41f2533288f8
Matīss Ceplītis <matissceplitis051@gmail.com>
2021-08-18 15:03:03 +0300

    Exercise4 not done

---
+++ ClassesAndObjects/Exercise4/Movies.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    class Movies
+    {
+        string _title;
+        string _studio;
+        string _rating;
+        public Movies(string title, string studio, string rating)
+        {
+            _title = title;
+            _studio = studio;
+            _rating = rating;
+        }
+        public Movies(string title, string studio)
+        {
+            _title = title;
+            _studio = studio;
+        }
+        public string[] GetPG(string[] arr)
+        {
+            return arr;
+        }
+    }
+}
---
+++ ClassesAndObjects/Exercise4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstMovie = new Movies("Casino Royale","Eon Productions","PG­13");
+            var secondMovie = new Movies("Glass", "Buena Vista International", "PG­13");
+            var thirdMovie = new Movies("Spider-Man: Into the Spider-Verse", "Columbia Pictures", "PG");
+
+            
+        }
+    }
+}
