352321196f437db94b9287a28df8a7fa71c7c904
Matīss Ceplītis <matissceplitis051@gmail.com>
2021-08-20 19:35:14 +0300

    dogs

---
+++ ClassesAndObjects/Exercise7 Dog/Dog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7_Dog
+{
+    class Dog
+    {
+        string Name;
+        string Sex;
+    }
+}
---
+++ ClassesAndObjects/Exercise7 Dog/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise7_Dog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
