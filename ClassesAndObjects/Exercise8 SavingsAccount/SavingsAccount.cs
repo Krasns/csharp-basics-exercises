f6f75b685993c87665a70340a38870a275357e4b
Matīss Ceplītis <matissceplitis051@gmail.com>
2021-08-20 14:42:53 +0300

    Exercise8_SavingsAccount TotalInterestEarned not working as intended

---
+++ ClassesAndObjects/Exercise8 SavingsAccount/SavingsAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8_SavingsAccount
+{
+    class SavingsAccount
+    {
+        double Amount;
+        int TotalDeposited;
+        int TotalWithDrawn;
+        double TotalInterestEarned;
+        public SavingsAccount(int amount)
+        {
+            Amount = amount;
+        }
+        public void WithDrawal(int amountWithDrawn)
+        {
+            Amount -= amountWithDrawn;
+            TotalWithDrawn += amountWithDrawn;
+        }
+        public void Deposit(int amountDeposit) 
+        {
+            Amount += amountDeposit;
+            TotalDeposited += amountDeposit;
+        }
+        public void MonthlyInterest(double interestRate) 
+        {
+            Amount += (interestRate / 12) * Amount;
+            TotalInterestEarned += (interestRate / 12) * Amount;
+        }
+        public void TotalShown()
+        {
+            Console.WriteLine($"Total deposited: ${TotalDeposited}.00");
+            Console.WriteLine($"Total withdrawn: ${TotalWithDrawn}.00");
+            Console.WriteLine($"Interest earned: ${Math.Round(TotalInterestEarned,2)}");
+            Console.WriteLine($"Ending balance: ${Math.Round(Amount,2)}");
+        }
+    }
+}
