698226e072db9e1c7b262bf6c287ec22299f2aeb
Matīss Ceplītis <matissceplitis051@gmail.com>
2021-08-23 16:43:34 +0300

    Exercise5 WordCount done

--- Collections/WordCount/Program.cs
+++ Collections/WordCount/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,18 @@ namespace WordCount
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int countLines = File.ReadAllLines(@"D:\Codelex Lekcijas\csharp-basics-exercises\Collections\WordCount\text.txt").Length;
+
+            string text = File.ReadAllText(@"D:\Codelex Lekcijas\csharp-basics-exercises\Collections\WordCount\text.txt");
+            string[] textArray = text.Split(' ');
+            int countWords = textArray.Length;
+            
+            int countChar = File.ReadAllText(@"D:\Codelex Lekcijas\csharp-basics-exercises\Collections\WordCount\text.txt").Length;
+
+            Console.WriteLine($"Lines: {countLines}");
+            Console.WriteLine($"Words: {countWords}");
+            Console.WriteLine($"Chars: {countChar}");
+            Console.ReadLine();
         }
     }
 }
