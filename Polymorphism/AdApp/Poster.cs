c212a2ce952aa72d0efb1eeaea5cb06b9e9773b1
Matīss Ceplītis <matissceplitis051@gmail.com>
2021-08-27 16:45:45 +0300

    Exercise 5 AdApp done

---
+++ Polymorphism/AdApp/Poster.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdApp
+{
+    public class Poster : Advert
+    {
+        private int _height;
+        private int _width;
+        private int _copys;
+        private int _costPerCopy;
+        public Poster(int fee) : base(fee)
+        {
+
+        }
+
+        public Poster(int fee, int height, int width, int copys, int costPerCopy) :base(fee)
+        {
+            _height = height;
+            _width = width;
+            _copys = copys;
+            _costPerCopy = costPerCopy;
+        }
+
+        private new int Cost()
+        {
+            var fee = base.Cost();
+            fee += _copys * _costPerCopy;
+            return fee;
+        }
+
+        public override string ToString()
+        {
+            return $"\nPoster: Cost: {Cost()}";
+        }
+    }
+}
--- Polymorphism/AdApp/TVAd.cs
+++ Polymorphism/AdApp/TVAd.cs
@@ -2,18 +2,33 @@ namespace AdApp
 {
     public class TVAd: Advert
     {
+        private int _airTime;
+        private int _peakTime;
+        private bool _isPeakTime;
         public TVAd(int fee) : base(fee)
         {
         }
+
+        public TVAd (int fee, int airTime, int peakTime, bool isPeakTime = false) : base(fee)
+	    {
+            _airTime = airTime;
+            _peakTime = peakTime;
+            _isPeakTime = isPeakTime;
+	    }
+
         
         public new int Cost() 
         {
-            return base.Cost();
+            var fee = base.Cost();
+            var cost = (_airTime - _peakTime) * fee;
+            if (_isPeakTime)
+	            cost += _peakTime* fee * 2;
+            return cost;
         }
 
         public override string ToString() 
         {
-            return base.ToString();
+            return $"\nTVAd: Cost:{Cost()}";
         }
     }
 }
