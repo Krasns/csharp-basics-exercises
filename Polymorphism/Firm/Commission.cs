3d55f1184bd200af7b3338117cf9e16c2bb86d83
Matīss Ceplītis <matissceplitis051@gmail.com>
2021-10-12 18:31:02 +0300

    Exercise 4 Firm done

---
+++ Polymorphism/Firm/Commission.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Firm
+{
+    class Commission : Hourly
+    {
+        private double _totalSales;
+        private double _commission;
+    
+        public Commission(string eName, string eAddress, string ePhone, string socSecNumber, double rate, double commision) : base(eName,
+                eAddress, ePhone, socSecNumber, rate)
+        {
+            _commission = commision;
+            _totalSales = 0;
+        }
+        public void AddSales(double totalSales)
+        {
+            _totalSales += totalSales;
+        }
+        public override double Pay()
+        {
+            var payment = base.Pay() + ( _totalSales * _commission);
+            _totalSales = 0;
+            return Math.Round(payment,2);
+        }
+        public override string ToString()
+        {
+            var result = base.ToString();
+            return result;
+        }
+    }
+
+}
--- Polymorphism/Firm/Staff.cs
+++ Polymorphism/Firm/Staff.cs
@@ -11,7 +11,7 @@ namespace Firm
         //-----------------------------------------------------------------
         public Staff()
         {
-            staffList = new StaffMember[6];
+            staffList = new StaffMember[9];
             staffList[0] = new Executive("Sam", "123 Main Line",
                 "555-0469", "123-45-6789", 2423.07);
             staffList[1] = new Employee("Carla", "456 Off Line",
@@ -24,8 +24,20 @@ namespace Firm
                 "555-8374");
             staffList[5] = new Volunteer("Cliff", "321 Duds Lane",
                 "555-7282");
+            staffList[6] = new Commission("Beans", "321 Apex",
+                "555-5247", "958-47-3625", 6.25, 0.2);
+            staffList[7] = new Commission("Man", "Man Man",
+                "MAN-MAN", "MAN-MA-MANA", 9.75, 0.15);
+            staffList[8] = new Commission("Krûms", "Pie Meþa",
+                "555-5478", "952-52-4385", 21.42, 0.4);
             ((Executive) staffList[0]).AwardBonus(500.00);
             ((Hourly) staffList[3]).AddHours(40);
+            ((Commission) staffList[6]).AddHours(35);
+            ((Commission) staffList[6]).AddSales(400);
+            ((Commission) staffList[7]).AddHours(40);
+            ((Commission) staffList[7]).AddSales(950);
+            ((Commission) staffList[8]).AddHours(40);
+            ((Commission) staffList[8]).AddSales(1150);
         }
 
         //-----------------------------------------------------------------
